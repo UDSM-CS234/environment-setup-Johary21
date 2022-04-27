63497d24fdb54c4e33637bad835890d4142ec5bf
Johary21 <joharymterry@gmail.com>
2022-04-27 10:29:33 +0300

    lab1 Submission

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         double gravity = -9.81; // Earth's gravity in m/s^2
-        double initialVelocity = 0.0;
+        double initialVelocity = 0.00;
         double fallingTime = 10.0;
-        double initialPosition = 0.0;
+        double initialPosition = 0.00;
         double position = 0.5 * gravity* fallingTime*fallingTime + initialVelocity*fallingTime + initialPosition;
-        double finalPosition = 0.0;
+        double finalPosition = 0.00;
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + position + " m.");
     }
